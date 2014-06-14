78a03591639766f825a3bcd96ba1abe209f62ee4
Kurakin Alexander <kuraga333@mail.ru>
2014-06-14 12:40:27 +0400

    Let's don't watch

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,6 @@
 module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-karma');
-  grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
@@ -14,17 +13,8 @@ module.exports = function(grunt) {
         autoWatch: false,
         browsers: ['PhantomJS']
       }
-    },
-    watch: {
-      scripts: {
-        files: ['src/**/*.*', 'test/**/*.js', 'examples/**/*.html'],
-        tasks: ['copy:build', 'uglify:build'],
-        options: {
-          spawn: false,
-          interupt: true
-        }
-      }
-    },
+    }
+  });
 
   grunt.registerTask('unit', ['karma']);
 };
